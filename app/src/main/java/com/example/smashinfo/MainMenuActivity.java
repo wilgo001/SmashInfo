beed8ca50c5fc10041018f7026acc0cf83e8d9c0
wilgo001 <wilhelm.salles@outlook.fr>
2019-10-03 12:04:35 +0200

    commit V3 déconnexion

--- app/src/main/java/com/example/smashinfo/MainMenuActivity.java
+++ app/src/main/java/com/example/smashinfo/MainMenuActivity.java
@@ -2,13 +2,37 @@ package com.example.smashinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private Button buttonDeconnexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        buttonDeconnexion = (Button) findViewById(R.id.buttonDeconnexion);
+
+        buttonDeconnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                quitApp();
+            }
+        });
+
+    }
+
+    private void quitApp() {
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        startActivity(myIntent);
     }
 }
