1c06ff4daa59bed65f7f2db489b849e8853e5fb6
wilgo001 <wilhelm.salles@outlook.fr>
2019-11-19 12:23:09 +0100

    commit création partie

--- app/src/main/java/com/example/smashinfo/Activity/MainMenuActivity.java
+++ app/src/main/java/com/example/smashinfo/Activity/MainMenuActivity.java
@@ -6,13 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.smashinfo.R;
+import com.example.smashinfo.data.Partie;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainMenuActivity extends AppCompatActivity {
 
-    private Button buttonDeconnexion;
+    public static final String JOINER_NAME = "searching";
+    private Button buttonDeconnexion, createGame, loadGame;
+    private EditText pseudo;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +29,30 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         buttonDeconnexion = (Button) findViewById(R.id.buttonDeconnexion);
+        createGame = (Button) findViewById(R.id.createGame);
+        loadGame = (Button) findViewById(R.id.loadGame);
+        pseudo = (EditText) findViewById(R.id.pseudo);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         buttonDeconnexion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 quitApp();
+            }
+        });
+
+        createGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pseudo.getText().toString().equals("")) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "complete pseudo please", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+                Partie partie = new Partie(pseudo.getText().toString(), JOINER_NAME, "white");
+                mDatabase.child("parties").push().setValue(partie);
+                Toast toast = Toast.makeText(getApplicationContext(), "partie créée. en attente de joueur", Toast.LENGTH_LONG);
             }
         });
 
---
+++ app/src/main/java/com/example/smashinfo/data/Partie.java
@@ -0,0 +1,17 @@
+package com.example.smashinfo.data;
+
+public class Partie {
+
+    public String hosterName;
+    public String joinerName;
+    public String color;
+
+    public Partie() {
+    }
+
+    public Partie(String hosterName, String joinerName, String color) {
+        this.hosterName = hosterName;
+        this.joinerName = joinerName;
+        this.color = color;
+    }
+}
