32a877f199f0356edc29df48c3ebd8e4f2db39e5
wilgo001 <wilhelm.salles@outlook.fr>
2019-12-12 14:55:34 +0100

    il était une fois ... !

--- app/src/main/java/com/example/smashinfo/activity/FieldActivity.java
+++ app/src/main/java/com/example/smashinfo/activity/FieldActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smashinfo.R;
 import com.example.smashinfo.game.Player;
@@ -32,12 +33,12 @@ public class FieldActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field);
 
-        //Intent intent = this.getIntent();
-        //partieKey = intent.getStringExtra(MainMenuActivity.PARTIE_KEY);
+        Intent intent = this.getIntent();
+        partieKey = intent.getStringExtra(MainMenuActivity.PARTIE_KEY);
 
-        //Toast toast = Toast.makeText(getApplicationContext(), "key : " + partieKey, Toast.LENGTH_SHORT);
-        //toast.show();
-        /*
+        Toast toast = Toast.makeText(getApplicationContext(), "key : " + partieKey, Toast.LENGTH_SHORT);
+        toast.show();
+
         hideNavigationBar();
 
         mainButton = findViewById(R.id.main);
@@ -105,9 +106,9 @@ public class FieldActivity extends AppCompatActivity {
             }
         });
 
-*/
+
     }
-/*
+
     private void main() {
 
     }
@@ -133,5 +134,5 @@ public class FieldActivity extends AppCompatActivity {
     private void hideNavigationBar() {
         this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
- */
+
 }
