e1a8a1d66e0d71cf253a4dff2206ba8c145453cb
wilgo001 <wilhelm.salles@outlook.fr>
2019-12-13 03:11:41 +0100

    burp

--- app/src/main/java/com/example/smashinfo/data/DataCardsNames.java
+++ app/src/main/java/com/example/smashinfo/data/DataCardsNames.java
@@ -2,6 +2,6 @@ package com.example.smashinfo.data;
 
 public enum DataCardsNames {
 
-    Alexandre_Clouet, Antoine_Jonard, Charles_Caillon, Colin_Bouligand, Corentin_Dizel, Corentin_Livain,
+    Alexandre_Clouet, Antoine_Jonard, Charles_Caillon, Colin_Bouligand, Corentin_Dizel, Corentin_Livain, Grégoire_Bellon,
 
 }
