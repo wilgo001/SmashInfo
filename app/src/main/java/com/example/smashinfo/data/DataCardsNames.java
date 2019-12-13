5491265c904372214ff67b8d7fc316c392872d37
Corentin <i181624>
2019-12-13 03:50:03 +0100

    le sommeil

--- app/src/main/java/com/example/smashinfo/data/DataCardsNames.java
+++ app/src/main/java/com/example/smashinfo/data/DataCardsNames.java
@@ -2,6 +2,19 @@ package com.example.smashinfo.data;
 
 public enum DataCardsNames {
 
-    Alexandre_Clouet, Antoine_Jonard, Charles_Caillon, Colin_Bouligand, Corentin_Dizel, Corentin_Livain, Grégoire_Bellon,
-
+    Alexandre_Clouet, Antoine_Jonard, Charles_Caillon, Colin_Bouligand,
+    Corentin_Dizel, Corentin_Livain, Grégoire_Bellon, Léo_Brunet,
+    Louis_Euvrard, Maxime_Maurin, Simon_Beaussier, Simon_Dubois,
+    Alex_Leborgne, Loris_Ponroy, Flavien_Robert, Valentin_Rouxel,
+    Samuel_Brouard, Red_Uni_IA_Ponroy, N2C, Victor_Le_Moan,
+    Clément_Laborie, Lucas_Guideau, Nathan_Le_Moan, Nicolas_Durand,
+    Quentin_Couland, Rafaël_Doneau, Teddy_Sicard, Valentin_Hallet,
+    Président_Sicard, William_Lechat, Wilhelm_Salles, Thomas_Lhermelin,
+    Cynthia_Dubile, Yanis_Saupin, Big_Brain, Salomé_Pechin,
+    Iza_Marfisi, Thomas_Duchemin, Clément_Boussard, Yazid_Kherrati,
+    Julian_Aubert, Gabriel_Edde, Alexandre_Baron, Étienne_Seniow,
+    Nathalie_Vieillard, Mamie_Vieillard, Cecile_Lucas, Yann_Walkowiak,
+    Axel_Blanchard, Nathan_Lefort, Léo_Malet, Tony_Delhommais,
+    Nathan_Chambrin, NTCH, Quentin_Fourier, Timothée_Griveau,
+    Gilles_Bancel, Axel_Coulon,
 }
